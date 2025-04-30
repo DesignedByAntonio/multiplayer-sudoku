327091ade49f807b6d0140b11f414aa035173daa
Antonio Medina <antoniomedina@Antonios-MacBook-Pro.local>
2025-04-29 22:37:40 -0700

    Connect socket.io to deployed server

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,9 @@
 import { io } from 'socket.io-client';
 
 // point this at your server URL
-const SERVER_URL = 'http://localhost:4000';
+// const SERVER_URL = 'http://localhost:4000';
+const SERVER_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000';
+
 
 // auto-connect
 const socket = io(SERVER_URL, {
